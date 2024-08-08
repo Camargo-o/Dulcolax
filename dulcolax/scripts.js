7002c9a3b7ab299d5d5d99a947a06472a1d5264b
camargo-o <lucas.camargo@mrm.com>
2024-08-08 14:15:54 -0300

    add iris

--- dulcolax/scripts.js
+++ dulcolax/scripts.js
@@ -40,3 +40,25 @@ document.addEventListener('DOMContentLoaded', () => {
     updateCarrossel();
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mobile = document.querySelector('.mobile');
+    const nav = document.querySelector('.menu');
+
+    mobile.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        mobile.classList.toggle('toggle');
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.linguagem a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            links.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
+
